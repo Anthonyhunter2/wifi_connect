2f30ba82c7d6884b5907128615b378d1977138cf
Anthony <anthonyhunter2@hotmail.com>
2017-07-16 21:05:06 -0400

    Updated code

--- connect_to_wireless.py
+++ connect_to_wireless.py
@@ -20,17 +20,19 @@ def write_info_to_tmpfile():
 	gather_info_from_tmpfile()
 
 def gather_info_from_tmpfile():
-	possible = OrderedDict({})
 	file_name = open("testfile.tmp","r")
 	grouped_up = []
-	count = 1
 	for line in file_name.readlines():
 		line = line.replace(":"," ")
 		line = line.split(" ")
 		line = list(filter(None, line))
-		if "Address" in line or "ESSID" in line or "Frequency" in line:
+		if "Address" in line or "ESSID" in line or "Frequency" in line or "Encryption" in line:
 			grouped_up.append(line)
-	for item in grouped_up:	
+	parse_list(grouped_up)
+def parse_list(given_list):
+	count = 1
+	possible = OrderedDict({})
+	for item in given_list:	
 		key = "Choice "+str(count)+":"
 		if "Address" in item:
 			address = ":".join(item[4:])
@@ -47,9 +49,29 @@ def gather_info_from_tmpfile():
 			possible.setdefault(key, []) 
 			possible[key].append("ESSID, "+essid)
 			count = count +1 
+		elif "Encryption" in item:
+			enc = item[2].split('\n')[0]
+			if enc == 'on':
+				enc = "Yes"
+				possible.setdefault(key, []) 
+				possible[key].append("Password Required")
+			else:
+				enc = "No"
+				possible.setdefault(key, []) 
+				possible[key].append("No Password Required")
 	for choice in possible:
-		print(choice, possible[choice])
+		print(choice, possible[choice][3].split(",")[1], possible[choice][2])
+	action_on_answer(possible)
 	os.remove("testfile.tmp")
-
+def action_on_answer(dictonary):
+	all_choices = []
+	connect = input("Which Wifi Network would you like to connect to ? (Just Enter The Number): ")
+	for answer in dictonary:
+		all_choices.append(answer)
+	if str("Choice " +connect+ ":") in all_choices:
+		print(answer, dictonary[answer])
+	else:
+		print("Sorry I don't see that as a possible Choice")
 #gather_info_from_tmpfile()
 write_info_to_tmpfile()
+#action_on_choice()
