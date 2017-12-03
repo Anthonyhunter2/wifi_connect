c5da23272166464044cd2a70030cd2112c5e559f
Anthony <anthonyhunter2@hotmail.com>
2017-12-03 09:12:54 -0500

    Changed this script to use classes, and only display 1 instance of each wifi name

--- connect_to_wireless.py
+++ connect_to_wireless.py
@@ -5,78 +5,128 @@ import os
 import subprocess
 from collections import OrderedDict
 from sys import exit
-def check_for_wifi_card():
-	dir_to_check = "/sys/class/net"
-	list_dir = os.listdir(dir_to_check)
-	for item in list_dir:
-		if search('^w.*', item):
-			return item
+import getpass
+class Network():
+	def __init__(self, name):
+		self.name = name
+		self.card = self.check_for_wifi_card()
+	def check_for_wifi_card(self):
+		dir_to_check = "/sys/class/net"
+		list_dir = os.listdir(dir_to_check)
+		for item in list_dir:
+			if search('^w.*', item):
+				return(item)
+	def connect_known(self):
+		ifup = subprocess.Popen(["nmcli", "c", "up", "id", self.name])
+		ifup.communicate()
+	def connect_nopass(self):
+		first = subprocess.Popen(["nmcli", "con", "add", "con-name", self.name, "ifname", self.card, "type", "wifi", "ssid", self.name])
+		first.communicate()
+		second = subprocess.Popen(["nmcli", "c", "up", "id", self.name])
+		second.communicate()
+	def connect_unknown(self):
+		first = subprocess.Popen(["nmcli", "con", "add", "con-name", self.name, "ifname", self.card, "type", "wifi", "ssid", self.name])
+		first.communicate()
+		self.password()
+		self.connect_known()
+	def disconnect(self):
+		discon = subprocess.Popen(["nmcli", "c", "down", "id", self.name])
+		discon.communicate()
+	def password(self):
+		password = getpass.getpass("Please Enter the Password for {}: ".format(self.name))
+		first = subprocess.Popen(["nmcli", "con", "modify", self.name, "wifi-sec.key-mgmt", "wpa-psk"])	
+		first.communicate()
+		second = subprocess.Popen(["nmcli", "con", "modify", self.name, "wifi-sec.psk", password])
+		second.communicate()
+	
 def output_of_wifi_scan():
 	scan_router = subprocess.Popen(["nmcli", "dev", "wifi", "list" ], stdout=subprocess.PIPE)
-	grouped_up = []
+	already_in_dict = []
 	possible = OrderedDict({})
 	count = 1
 	for line in scan_router.stdout.readlines():
-		if b'Infra' in line:
-			grouped_up.append(line.decode("utf-8").strip("*").strip("\n").strip(" "))
-	for line in grouped_up:
-		key = "Choice "+str(count)+":"
-		line = line.split()
-		full_name = " ".join(line[0:line.index("Infra")])
-		security = line[-1]
-		possible.setdefault(key, []) 
-		possible[key].append(full_name)
-		possible[key].append(security)
-		count += 1
-	for key in possible:
-		if possible[key][1] == "--":
-			print(key, possible[key][0], "No Password")
+		line = line.decode("utf-8").strip("*").split()
+		if "SSID" not in line:
+			key = "Choice "+str(count)+":"
+			full_name = " ".join(line[0:line.index("Infra")])
+			if line[-1:] == "--":
+				security = "\033[32mNo Password\033[0m"
+			else:
+				security = "\033[31mPassword Required\033[0m"
+			value = "{}: {}".format(full_name, security)
+			if full_name not in already_in_dict:
+				already_in_dict.append(full_name)	
+				possible[key] = value
+				count += 1
+	return(possible)
+def are_you_connected():
+	scan_router = subprocess.Popen(["nmcli", "dev", "wifi", "list" ], stdout=subprocess.PIPE)
+	for line in scan_router.stdout.readlines():
+		line = line.decode("utf-8").split()
+		if "SSID" not in line:
+			if line[0] == "*":
+				full_name = " ".join(line[1:line.index("Infra")])
+				return(full_name)
+
+def maybe_connected():
+	affirm = ["yes", "Yes", "y", "Y"]
+	negitive = ["No", "no", "N", "n"]
+	connected = are_you_connected()
+	if connected:
+		print("You are connected to \"{}\"".format(connected))
+		disconnect = input("Would you like to disconnect from \"{}\"  y|n\n".format(connected))
+		if disconnect in affirm:
+			wifi = Network(connected)
+			wifi.disconnect()
+			exit()
+		elif disconnect in negitive:
+			pass
 		else:
-			print(key, possible[key][0], possible[key][1])
-	return possible
-def action_on_answer(dictonary, network_card):
-	all_choices = []
-	connect = input("Which Wifi Network would you like to connect to ? (Just Enter The Number): ")
-	for answer in dictonary:
-		all_choices.append(answer)
-		choice_string = "Choice {}:".format(connect)
-	if choice_string in all_choices:
-		is_it_here = []
-		network_id = dictonary[choice_string][0]
-		checking_for_previous_connaction = os.listdir("/etc/NetworkManager/system-connections")
-		for item in checking_for_previous_connaction:
-			is_it_here.append(item)
-		if network_id in is_it_here:	
-				response = input("Would you like to be connected to {} ? y|n ".format(network_id))
-				if response == "y"or response == "Y" or response == "yes":
-					connect = subprocess.Popen(["nmcli", "c", "up", "id", network_id], stdout=subprocess.PIPE)
-					for line in connect.stdout.readlines():
-						print(line.decode("utf-8"))
-				elif response == "n" or response == "N" or response == "no":
-					connect = subprocess.Popen(["nmcli", "c", "down", "id", network_id], stdout=subprocess.PIPE)
-					for line in connect.stdout.readlines():
-						print(line.decode("utf-8"))
-				else:
-					print("Sorry I did not understand please use, y or n")	
-		else:
-			if "No Password" not in dictonary[choice_string]:
-				print("This Seems to be a new connection")
-				command = subprocess.Popen(["nmcli", "con", "add", "con-name", network_id, "ifname", network_card, "type", "wifi", "ssid", network_id], stdout=subprocess.PIPE)
-				for line in command.stdout.readlines():
-					print(line.decode("utf-8"))
-				add_password = subprocess.Popen(["nmcli", "con", "modify", network_id, "wifi-sec.key-mgmt", "wpa-psk"], stdout=subprocess.PIPE)
-				for line in add_password.stdout.readlines():
-					print(line.decode("utf-8"))
-				password = input("Please Enter the Password for {}: ".format(network_id))
-				give_password = subprocess.Popen(["nmcli", "con", "modify", network_id, "wifi-sec.psk", password], stdout=subprocess.PIPE)
-				for line in give_password.stdout.readlines():
-					print(line.decode("utf-8"))
-				connect = subprocess.Popen(["nmcli", "c", "up", "id", network_id], stdout=subprocess.PIPE)
-				for line in connect.stdout.readlines():
-					print(line.decode("utf-8"))
+			print("Sorry please use \"y\" or \"n\"")
+			maybe_connected()
+def more_options_fun():
+	affirm = ["yes", "Yes", "y", "Y"]
+	negitive = ["No", "no", "N", "n"]
+	save = []
+	possible_connections = output_of_wifi_scan()
+	for choices in possible_connections.items():
+		print(" ".join(choices))
+	more_options = input("\033[1mWould you like to connect to one of the networks above? y|n \033[0m\n")
+	if more_options in affirm:
+		answ = input("Which one? Just the number of your choice ie 2\n")
+		for key, value in possible_connections.items():
+			if answ in key:
+				save.append(value.split(":")[0])
+	elif more_options in negitive:
+		print("Well then why are you here? Good Day Sir!.... \033[1;31mI said GOOD DAY\033[0m")
+		exit()
+	elif int(more_options) in range(len(possible_connections)):
+		for key, value in possible_connections.items():
+			if more_options in key:
+				save.append(value.split(":")[0])
 	else:
-		print("Sorry I don't see that as a possible Choice")
-
-
-
-action_on_answer(output_of_wifi_scan(), check_for_wifi_card())
+		print("Sorry I didn't see that option please try again")
+		more_options_fun()
+	if save:
+		net = " ".join(save)
+		if see_if_connected_before(net):
+			conn = Network(net)
+			conn.connect_known()
+		elif not see_if_connected_before(net):
+			for key, value in possible_connections.items():
+				if net in value:
+					if "No Password" in value:
+						conn = Network(net)
+						conn.connect_nopass()
+					elif "Required" in value:
+						conn = Network(net)
+						conn.connect_unknown()
+def see_if_connected_before(past_con):
+	check_dir = "/etc/NetworkManager/system-connections"
+	in_here = os.listdir(check_dir)
+	if past_con in in_here:
+		return(past_con)
+def main():
+	maybe_connected()
+	more_options_fun()
+main()
